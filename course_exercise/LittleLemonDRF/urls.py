dcc9c6ab1531c7fb0147c44aca96d9f9c1bce7e6
Raju Tamang <ezycom102@gmail.com>
2024-10-11 18:56:55 +0545

    module 3 01-labwork code uploaded

--- course_exercise/LittleLemonDRF/urls.py
+++ course_exercise/LittleLemonDRF/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('ratings', views.RatingsView.as_view()),
-    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
-
--- course_exercise/LittleLemonDRF/views.py
+++ course_exercise/LittleLemonDRF/views.py
@@ -2,13 +2,22 @@
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+# from rest_framework.decorators import api_view
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    ordering_fields = ['price', 'inventory']
+    search_fields=['title']
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    
+
+""" 
+# Function based views
+@api_view(['GET'], ['POST'])
+def menu_items(request):
+    if(request.method == 'GET'):
+ """
